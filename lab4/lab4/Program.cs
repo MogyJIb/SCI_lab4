0858906a062894f897b7224744298b9d96ea3896
MogyJIb <zhenya_861533@tut.by>
2018-04-08 19:14:49 +0300

    load project

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using lab4.Utils;
+using lab4;
+
+namespace lab3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DbInitializationUtil.Init();
+
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
